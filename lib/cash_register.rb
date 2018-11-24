1541b74a9ab5f17f4c70beb04a198966fa9220ae
Katie Ashlin <akka726@outlook.com>
2018-11-24 23:38:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  
+  attr_accessor :total, :items
+  
+  def initialize(total = 0)
+    @total = total
+    @items = []
+  end
+  
+  def discount
+    @total
+  end
+  
+  def add_item(title, price, quantity= 1)
+    self.total += quantity * price
 
+    end
+  end
+  
+ 
+end
